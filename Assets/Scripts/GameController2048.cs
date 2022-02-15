a6096bf1c70774f96cef1cb90e890648618b3518
Landon Gelven <lgelven@micds.org>
2022-02-15 10:53:55 -0600

    Need to fix if statement

--- Assets/Scripts/GameController2048.cs
+++ Assets/Scripts/GameController2048.cs
@@ -9,7 +9,10 @@ using System;
 public class GameController2048 : MonoBehaviour
 {
     public static GameController2048 instance;
-    public static int ticker; 
+    public static int ticker;
+    public static int movementTicker;
+
+    
 
     [SerializeField] GameObject fillPrefab;
     [SerializeField] Transform[] allCells;
@@ -25,8 +28,8 @@ public class GameController2048 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartSpawnFill();
-        StartSpawnFill();
+        SpawnFill();
+        SpawnFill();
     }
 
     // Update is called once per frame
@@ -68,44 +71,59 @@ public class GameController2048 : MonoBehaviour
             SlideLeft(allCells[12].GetComponent(typeof(Cell2048)) as Cell2048); //Pass the top left cell in 
             
         }
+        if (movementTicker != 0)
+            SpawnFill();
+        movementTicker = 0;
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             DebugSpawnFill();
         }
     }
+
+    //Need to find a way to choose the values that are going to be able to spawn. Then use the random generator to pick one of those values. 
     public void SpawnFill()
     {
-        int whichSpawn = UnityEngine.Random.Range(0, allCells.Length);
-        if(allCells[whichSpawn].childCount != 0)
+        ArrayList cellCounter = new ArrayList();
+        for (int i = 0; i<allCells.Length; i++)
         {
-            Debug.Log(allCells[whichSpawn].name + " is already filled");
-            SpawnFill();
-            return; 
+            if(allCells[i] == null){
+                //This if statement does not work. The cells are not null?
+                Debug.Log("blah");
+                cellCounter.Add(i);
+            }
         }
+        int spawnIndexOfArray = UnityEngine.Random.Range(0, cellCounter.Count);
+        Debug.Log(spawnIndexOfArray);
+        int whichSpawn = (int)cellCounter[spawnIndexOfArray];
+
+
         float chance = UnityEngine.Random.Range(0f, 1f);
-        Debug.Log("The random number was "+chance);
-        /*if (chance < .2f)
-        {
+            //Debug.Log("The random number was "+chance);
+            /*if (chance < .2f)
+            {
+                return;
+            }
+            */
+            if (chance < .5f)
+            {
+                GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
+                //Debug.Log("Becuase the random number was " + chance + " we spawned a 2");
+                Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
+                allCells[whichSpawn].GetComponent<Cell2048>().fill = tempFillComp;
+                tempFillComp.FillValueUpdate(2);
+            }
+            else
+            {
+                GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
+                //Debug.Log("Becuase the random number was " + chance + " we spawned a 4");
+                Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
+                allCells[whichSpawn].GetComponent<Cell2048>().fill = tempFillComp; 
+                tempFillComp.FillValueUpdate(4); 
+            }
             return;
-        }
-        */
-        if (chance < .5f)
-        {
-            GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
-            Debug.Log("Becuase the random number was " + chance + " we spawned a 2");
-            Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
-            allCells[whichSpawn].GetComponent<Cell2048>().fill = tempFillComp;
-            tempFillComp.FillValueUpdate(2);
-        }
-        else
-        {
-            GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
-            Debug.Log("Becuase the random number was " + chance + " we spawned a 4");
-            Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
-            allCells[whichSpawn].GetComponent<Cell2048>().fill = tempFillComp; 
-            tempFillComp.FillValueUpdate(4); 
-        }
     }
+        
+    
 
     public void DebugSpawnFill()
     {
@@ -126,12 +144,12 @@ public class GameController2048 : MonoBehaviour
         int whichSpawn = UnityEngine.Random.Range(0, allCells.Length);
         if (allCells[whichSpawn].childCount != 0)
         {
-            Debug.Log(allCells[whichSpawn].name + " is already filled");
+            //Debug.Log(allCells[whichSpawn].name + " is already filled");
             SpawnFill();
             return;
         }
             GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
-            Debug.Log(2);
+            //Debug.Log(2);
             Fill2048 tempFillComp = tempFill.GetComponent<Fill2048>();
             allCells[whichSpawn].GetComponent<Cell2048>().fill = tempFillComp;
             tempFillComp.FillValueUpdate(2);       
@@ -151,11 +169,12 @@ public class GameController2048 : MonoBehaviour
             }
             if (currentCell != null && currentCell.fill != null)
             {
-                Debug.Log("Attempting to change to " + currentCell.fill.value);
+                //Debug.Log("Attempting to change to " + currentCell.fill.value);
                 headValue.fill = makeCell(currentCell.fill.value, headValue.postionInArray);
                 headValue.fill.FillValueUpdate(currentCell.fill.value);
                 currentCell.fill.removeFromBoard();
                 currentCell.fill = null;
+                movementTicker++;
             }
         }
 
@@ -175,7 +194,7 @@ public class GameController2048 : MonoBehaviour
                     headValue.fill.FillValueUpdate(headValue.fill.value);
                     currentCell.fill.removeFromBoard();
                     currentCell.fill = null;
-
+                    movementTicker++;
                 }
             }
             
@@ -184,6 +203,8 @@ public class GameController2048 : MonoBehaviour
         {
             SlideUp((headValue.GetComponent(typeof(Cell2048)) as Cell2048).down);
         }
+        
+        
     }
     private void SlideRight(Cell2048 headValue)
     {
@@ -200,11 +221,12 @@ public class GameController2048 : MonoBehaviour
             }
             if (currentCell != null && currentCell.fill != null)
             {
-                Debug.Log("Attempting to change to " + currentCell.fill.value);
+                //Debug.Log("Attempting to change to " + currentCell.fill.value);
                 headValue.fill = makeCell(currentCell.fill.value, headValue.postionInArray);
                 headValue.fill.FillValueUpdate(currentCell.fill.value);
                 currentCell.fill.removeFromBoard();
                 currentCell.fill = null;
+                movementTicker++;
             }
         }
 
@@ -224,8 +246,9 @@ public class GameController2048 : MonoBehaviour
                     headValue.fill.FillValueUpdate(headValue.fill.value);
                     currentCell.fill.removeFromBoard();
                     currentCell.fill = null;
-
+                    movementTicker++;
                 }
+                
             }
 
         }
@@ -233,6 +256,7 @@ public class GameController2048 : MonoBehaviour
         {
             SlideRight((headValue.GetComponent(typeof(Cell2048)) as Cell2048).left);
         }
+        
     }
 
     private void SlideDown(Cell2048 headValue)
@@ -250,11 +274,12 @@ public class GameController2048 : MonoBehaviour
             }
             if (currentCell != null && currentCell.fill != null)
             {
-                Debug.Log("Attempting to change to " + currentCell.fill.value);
+                //Debug.Log("Attempting to change to " + currentCell.fill.value);
                 headValue.fill = makeCell(currentCell.fill.value, headValue.postionInArray);
                 headValue.fill.FillValueUpdate(currentCell.fill.value);
                 currentCell.fill.removeFromBoard();
                 currentCell.fill = null;
+                movementTicker++;
             }
         }
 
@@ -274,7 +299,7 @@ public class GameController2048 : MonoBehaviour
                     headValue.fill.FillValueUpdate(headValue.fill.value);
                     currentCell.fill.removeFromBoard();
                     currentCell.fill = null;
-
+                    movementTicker++;
                 }
             }
 
@@ -283,6 +308,7 @@ public class GameController2048 : MonoBehaviour
         {
             SlideDown((headValue.GetComponent(typeof(Cell2048)) as Cell2048).up);
         }
+        
     }
 
     private void SlideLeft(Cell2048 headValue)
@@ -300,11 +326,12 @@ public class GameController2048 : MonoBehaviour
             }
             if (currentCell != null && currentCell.fill != null)
             {
-                Debug.Log("Attempting to change to " + currentCell.fill.value);
+                //Debug.Log("Attempting to change to " + currentCell.fill.value);
                 headValue.fill = makeCell(currentCell.fill.value, headValue.postionInArray);
                 headValue.fill.FillValueUpdate(currentCell.fill.value);
                 currentCell.fill.removeFromBoard();
                 currentCell.fill = null;
+                movementTicker++;
             }
         }
 
@@ -324,7 +351,7 @@ public class GameController2048 : MonoBehaviour
                     headValue.fill.FillValueUpdate(headValue.fill.value);
                     currentCell.fill.removeFromBoard();
                     currentCell.fill = null;
-
+                    movementTicker++; 
                 }
             }
 
@@ -333,6 +360,7 @@ public class GameController2048 : MonoBehaviour
         {
             SlideLeft((headValue.GetComponent(typeof(Cell2048)) as Cell2048).right);
         }
+        
     }
     public Fill2048 makeCell(int valueToFill, int positionInGrid)
     {
